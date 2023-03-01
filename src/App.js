fa61ead0fcfb0e24f1b7cd888906bc77fa33950b
Kirdandan <elvinaxmedov9r22015@gmail.com>
2023-03-01 12:09:43 +0400

    #

--- src/App.js
+++ src/App.js
@@ -20,12 +20,18 @@ class App extends Component {
       );
   }
 
-  
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
     const filteredMonsters = this.state.monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(this.state.searchField);
     });
-    console.log(filteredMonsters)
+    console.log(filteredMonsters);
 
     return (
       <div className="App">
@@ -33,12 +39,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="Search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={this.onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
